86ea2850ec6e043df46960f6235a1683639a4f78
Pedro-Calderon <s18120164@alumnos.itsur.edu.mx>
2024-02-06 23:02:39 -0600

    APP de MarsPhotos se terminaron los primeros pasos pero hubo problemas en las pruebas.

---
+++ app/src/main/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import junit.framework.Assert.assertEquals
+import org.testng.annotations.Test
+
+class NetworkMarsRepositoryTest {
+  
+
+}
